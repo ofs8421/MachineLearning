c8a2704d77be27748db324389b09a1851c23060f
Olivier <54278151+ofs8421@users.noreply.github.com>
2019-08-19 12:14:03 -0400

    started simple chatbot
    i started making a chatbot

---
+++ SimpleChat/SimpleChat.py
@@ -0,0 +1,18 @@
+def processInput(iserInput):
+    pass 
+
+def main():
+    print("Welcome to video game facts! I can tell you about vidio games!\n")
+    print("Ask me a question or type in stop\n")
+
+    userInput = ""
+
+    while userInput != "quit":
+        userInput = input("what is your question? ").lower()
+        print(userInput)
+
+
+
+
+main()
+
