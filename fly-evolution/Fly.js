038b9f1568d1915c97cd200aca8aaf7e4c177a6d
Olivier <54278151+ofs8421@users.noreply.github.com>
2019-08-21 15:36:16 -0400

    i made a fly game

--- fly-evolution/Fly.js
+++ fly-evolution/Fly.js
@@ -1,28 +1,88 @@
 class Fly {
-    constructor(lifeSpan) {
+    constructor(lifeSpan, reward, punishment, food) {
         this.pos = createVector(width/2, height - 40);
         this.acc = createVector();
         this.vel = createVector();
         this.dna = new DNA(lifeSpan);
+        this.reward = reward;
+        this.punishment = punishment;
+        this.food = food;
+        this.fitness = 0;
+        this.hitSomething = false;
+        this.gotFood = false
+
+        this.searchingColor = color(0, 0, 0, 150);
+        this.foundColor = color(66, 245, 123, 150);
+    
+    
+    }   
+    
+    calcFitness(){
+        //fitness
+        let distance = dist(this.pos.x, this.pos.y, this.food.pos.x, this.food.pos.y);
+        this.fitness = map(distance, 0 , width, width, 0);
+
+        //apply Modifiers
+        if(distance <= this.food.radius){
+            this.fitness *= this.reward;
+    
+        }
+
+        if (this.hitSomething) {
+            this.fitness /= this.punishment;
+
+        }
     }
     
-    update(count){
-        this.applyForce(this.dna.genes[count]);
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
+    
+    
+    update(count, wall){
+        if(this.pos.y > height || this.pos.y < 0 || this.pos.x > width || this.pos.x < 0){
+            this.hitSomething = true;
+        }
+    
+        if(wall.hitWall(this.pos.x, this.pos.y)){
+            this.hitSomething = true;
+        }
+    
+        let distance = dist(this.pos.x, this.pos.y, this.food.pos.x, this.food.pos.y);
+        if(distance < this.food.radius){
+            this.gotFood = true;
+            this.hitSomething = true;
+        }
+    
+        if(!this.hitSomething){
+
+            this.applyForce(this.dna.genes[count]);
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+        }
+    
+        this.calcFitness();
     }
 
-    applyForce(force){
+
+
+    applyForce(force) {
         this.acc.add(force);
     }
-
+    
     show(){
         push();
         noStroke();
         translate(this.pos.x, this.pos.y);
+        fill(255),
+        textSize(10);
+        text(this.fitness.toFixed(1), 0, 0);
         rotate(this.vel.heading());
-        fill(82, 255, 113, 150);
+
+        if(this.gotFood){
+            fill(this.foundColor);
+        }else{
+            fill(this.searchingColor);
+        }
+        fill(0, 0, 0, 150);
         rectMode(CENTER);
         rect(0, 0, 25, 15);
         pop();
--- fly-evolution/sketch.js
+++ fly-evolution/sketch.js
@@ -1,35 +1,48 @@
-const LIFE_SPAN = 600; // How long do flies live
+const LIFE_SPAN = 700; // How long do flies live
 const POP_SIZE = 500; // How many flies do we want
-const REWARD_MULT = 10; // What is the reward for finding food
+const REWARD_MULT = 200; // What is the reward for finding food
 const PUNISH_DIV = 3; // What is the punishment for hitting something
 const MUTATION_RATE = 0.1; // What is the rate at which flies mutate
 
 let count = 0;
-
-let fly, food, wall;
+let generation = 0;
+let averageFit = 0;
+let sucsessRate = 0;
 
 function setup() {
     console.log('Hello');
 
     createCanvas(640, 480);
-
-    fly = new Fly(LIFE_SPAN);
-    food = new Food(width/2, 50, 30);
+   
+    population = new Population(LIFE_SPAN, POP_SIZE, REWARD_MULT, PUNISH_DIV);
+    
     wall = new Wall(width/2, height - height/3, 300, 30);
 }
 
 function draw() {
     background(3, 161, 252);
     
-    fly.update(count);
-    fly.show();
-
-    food.show();
-    wall.show();
     
+    fill(255)
+    textSize(20);
+    text("Generation:" + generation, 50, 50);
+    text("Average Fitness:" + averageFit, 50, 100);
+    text("Sucsess Rate:" + sucsessRate + "%", 50, 150);
+    
+    population.run(count);
     count++;
 
     if (count == LIFE_SPAN) {
+        population.evaluate();
+        
+        averageFit = population.findAverageFitness();
+
+        let newflies = population.generateNewPopulation(MUTATION_RATE);
+        sucsessRate = population. sucsessRate;
+
+        population = new Population(LIFE_SPAN, POP_SIZE, REWARD_MULT, PUNISH_DIV, newflies);
+        
         count = 0;
-    }
+        generation++;
+    }  
 }
