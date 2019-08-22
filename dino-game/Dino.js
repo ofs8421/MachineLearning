a58570a6a668effa038909fdcc76fa2c73b3b6c6
Olivier <54278151+ofs8421@users.noreply.github.com>
2019-08-22 17:16:21 -0400

    Dino Game Updates

--- dino-game/Dino.js
+++ dino-game/Dino.js
@@ -1,7 +1,99 @@
 class Dino {
-    constructor() {
+    constructor(playerControlled) {
+        this.originalwidth = 30;
+        this.originalheight = 50;
+        this.width = this.originalwidth;
+        this.height = this.originalheight;
 
+        this.pos = createVector(width / 4, this.calcGroundPosition());
+        this.vel = createVector();
+
+        this.playerControlled = playerControlled;
+    
+        this.isAlive = true;
+
+        //this.brain = brain;
     }
+
+    calcGroundPosition(){
+        return (height - height / 4 - this.height / 2);
+    }
+
+    update(closestCactus){
+        this.vel.y += -0.6;
+        this.pos.y -=this.vel.y;
+
+        let groundPos = this.calcGroundPosition();
+
+        if(!this.playerControlled){
+            // this.think(closestCactus);
+        }
+
+        if(this.pos.y >= groundPos){
+            this.pos.y = groundPos;
+            this.vel.y = 0;
+        }
+
+        if(this.hitCactus(closestCactus)){
+            this.isAlive = false;
+        }
+    }
+
+    think(cactus){
+
+        let distance = cactus.pos.x - cactusWidth / 2 - this.pos.x + this.width / 2;
+        
+        let actions = this.brain.predict([distance, cactus.count]);
+
+        let choice = actions.indexOf(Math.max(...actions));
+
+        // if(choice == 0){
+        //     this.unDuck();
+        //     this.jump();
+        // }else{
+        //     this.duck();
+        // }else{
+
+        //}
+        
+    }
+    
+    
+    jump(){
+        if(this.pos.y == this.calcGroundPosition() && this.height > this.originalwidth){
+            this.vel.y = 12; 
+        }
+    }
+
+
+    duck(){
+        if(this.pos.y == this.calcGroundPosition()){
+            this.height = this.originalwidth;
+            this.width = this.originalheight
+        }
+    }
+
+    unDuck(){
+        this.height = this.originalheight;
+        this.width = this.originalwidth;
+    }
+
+    show(){
+        push();
+        noStroke();
+        translate(this.pos.x, this.pos.y);
+        if(this.playerControlled){
+            fill(0, 0, 0, 100);
+        }else{
+            fill(66, 245, 126);        
+        }
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+       
+        
+    }
+    
 
     hitCactus(cactus) {
         let cactusWidth = ((cactus.width + cactus.spacing) * cactus.count) - cactus.spacing; // Calculate Full Width of Cactus
--- dino-game/sketch.js
+++ dino-game/sketch.js
@@ -1,15 +1,107 @@
 let ground;
 
+let dinos = [];
+let cacti = [];
 
+let spawnCactusFrame;
+
+let score = 0;
+let highScore = 0;
 
 function setup() {
     createCanvas(640, 480);
-
+    tf.setBackend('cpu')
     ground = new Ground();
+    spawnCactusFrame = 40;
+    cacti.push(new Cactus());
+
+    firstGeneration();
 }
 
 function draw() {
     background(255);
 
+    score += 1;
+
+    if (score > highScore){
+        highScore = score
+    }
+
+    push();
+    fill(0);
+    textSize(20);
+    textAlign(RIGHT);
+    text(score, width, 55)
+    text("HighScore: " + highScore, width, 30);
+    pop();
+
     ground.show();
+
+    if (frameCount == spawnCactusFrame){
+        cacti.push(new Cactus());
+        spawnCactusFrame += int(random(40, 60));
+    }
+
+    for(let i = 0; i < cacti.length; i++){
+        if (cacti[i].pos.x < -cacti[i].width){
+            cacti.shift();
+        }
+        cacti[i].update();
+        cacti[i].show();
+    }
+    
+    let allDead = true;
+
+    for(let i = 0; i < dinos.length; i++){
+        if (dinos[i].playerControlled){
+            if (keyIsDown(DOWN_ARROW)){
+                dinos[i].duck();
+            }else{
+                dinos[i].unDuck();
+            }
+            if(keyIsDown(UP_ARROW)){
+                dinos[i].jump();
+            }
+        }
+        
+        if (dinos[i].isAlive) {
+            allDead = false;
+            dinos[i].update(getClosestCactus(dinos[i]));
+            dinos[i].show();
+        }
+    }
+
+    if(allDead){
+        nextGeneration();
+    }
+
 }
+
+function getClosestCactus(dino){
+
+    let closestIndex = 0;
+
+    while(cacti[closestIndex].pos.x + cacti[closestIndex].fullWidth/2 < dino.pos.x - dino.width/2){
+        closestIndex++;
+    }
+    return cacti[closestIndex];
+}
+
+function firstGeneration(){
+    dinos = [];
+    dinos.push(new Dino(true, new NeuralNetwork(2, 4, 3)))
+}
+
+function nextGeneration(){
+    dinos = [];
+    dinos.push(new Dino(true, new NeuralNetwork(2, 4, 3)))
+
+    spawnCactusFrame = frameCount + 40;
+    cacti = [];
+    cacti.push(new Cactus());
+
+    score = 0;
+
+}
+
+
